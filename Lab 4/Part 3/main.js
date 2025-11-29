01870dde2858f945166f82e2095b7d18496de442
JerryK1746 <jerry.kwong@dcmail.ca>
2025-11-29 17:56:00 -0500

    Update main.js

--- Lab 4/Part 3/main.js
+++ Lab 4/Part 3/main.js
@@ -49,6 +49,19 @@ class Ball {
     this.x += this.velX;
     this.y += this.velY;
   }
+   collisionDetect() {
+    for (const ball of balls) {
+      if (this !== ball) {
+        const dx = this.x - ball.x;
+        const dy = this.y - ball.y;
+        const distance = Math.sqrt(dx * dx + dy * dy);
+
+        if (distance < this.size + ball.size) {
+          ball.color = this.color = randomRGB();
+        }
+      }
+    }
+  }
 }
 
 const testBall = new Ball(50, 100, 4, 4, "blue", 10);
@@ -57,4 +70,37 @@ testBall.x;
 testBall.size;
 testBall.color;
 testBall.draw();
+
+const balls = [];
+
+while (balls.length < 25) {
+  const size = random(10, 20);
+  const ball = new Ball(
+    // ball position always drawn at least one ball width
+    // away from the edge of the canvas, to avoid drawing errors
+    random(0 + size, width - size),
+    random(0 + size, height - size),
+    random(-7, 7),
+    random(-7, 7),
+    randomRGB(),
+    size,
+  );
+
+  balls.push(ball);
+}
+
+function loop() {
+  ctx.fillStyle = "rgb(0 0 0 / 25%)";
+  ctx.fillRect(0, 0, width, height);
+
+  for (const ball of balls) {
+    ball.draw();
+    ball.update();
+    ball.collisionDetect();
+  }
+
+  requestAnimationFrame(loop);
+}
+
+loop();
 
