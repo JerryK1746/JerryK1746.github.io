4aa609ac809efc25a22777e7f161c363c354bfc9
JerryK1746 <jerry.kwong@dcmail.ca>
2025-11-29 16:17:53 -0500

    Update main.js

--- Lab 4/Part 2/main.js
+++ Lab 4/Part 2/main.js
@@ -8,10 +8,10 @@ const overlay = document.querySelector(".overlay");
 
 const images = [
     { filename: "pic1.jpg", alt: "Closeup of a human eye"},
-    {filename: "pic2.jpg", alt: "Rock that looks like a wave"},
-    {filename: "pic3.jpg", alt: "Purple and white pansies"},
-    {filename: "pic4.jpg", alt: "Section of wall from a pharaoh's tomb"},
-    {filename: "pic5.jpg", alt: "Large moth on a leaf"},
+    { filename: "pic2.jpg", alt: "Rock that looks like a wave"},
+    { filename: "pic3.jpg", alt: "Purple and white pansies"},
+    { filename: "pic4.jpg", alt: "Section of wall from a pharaoh's tomb"},
+    { filename: "pic5.jpg", alt: "Large moth on a leaf"},
 ];
 // loop through images
 const baseURL = 
@@ -19,7 +19,7 @@ const baseURL =
 
 for (const image of images) {
     const newImage = document.createElemenet("img");
-    newImage.src = '${baseURL}${image.filename}';
+    newImage.src = `${baseURL}${image.filename}`;
     newImage.alt = image.alt;
     newImage.tabIndex = "0";
 
